d90c6a28ff0d292da00aa3702e6cd1af2b507936
Vattanak Samnang <vsamnang@paragoniu.edu.kh>
2026-03-06 23:48:50 +0700

    Initializing the GatherScholar project

---
+++ src/main/java/org/example/gatherscholar/GatherscholarApplication.java
@@ -0,0 +1,13 @@
+package org.example.gatherscholar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatherscholarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatherscholarApplication.class, args);
+    }
+
+}
