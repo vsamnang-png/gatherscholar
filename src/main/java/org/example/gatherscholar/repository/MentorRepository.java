51c9ee4c9acf431972328c79a6535e7f67c81458
Vattanak Samnang <vsamnang@paragoniu.edu.kh>
2026-03-07 21:32:33 +0700

    Add necessary dependency

---
+++ src/main/java/org/example/gatherscholar/repository/MentorRepository.java
@@ -0,0 +1,7 @@
+package org.example.gatherscholar.repository;
+
+import org.example.gatherscholar.entity.Mentor;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MentorRepository extends JpaRepository<Mentor, Long> {
+}
---
+++ src/main/java/org/example/gatherscholar/repository/ScholarshipRepository.java
@@ -0,0 +1,8 @@
+package org.example.gatherscholar.repository;
+
+import org.example.gatherscholar.entity.Scholarship;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ScholarshipRepository extends JpaRepository<Scholarship, Long> {
+
+}
