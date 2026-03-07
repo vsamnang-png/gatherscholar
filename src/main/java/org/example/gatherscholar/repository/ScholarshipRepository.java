10dd234efa3b1329111d0c37f4fa6a8dd572a6e1
Vattanak Samnang <vsamnang@paragoniu.edu.kh>
2026-03-07 23:46:06 +0700

    Correcting variable name

--- src/main/java/org/example/gatherscholar/repository/ScholarshipRepository.java
+++ src/main/java/org/example/gatherscholar/repository/ScholarshipRepository.java
@@ -3,6 +3,11 @@ package org.example.gatherscholar.repository;
 import org.example.gatherscholar.entity.Scholarship;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ScholarshipRepository extends JpaRepository<Scholarship, Long> {
 
+    List<Scholarship> findByCountry(String country);
+    List<Scholarship> findByProvider(String provider);
+    List<Scholarship> findByNameContaining(String name);
 }
