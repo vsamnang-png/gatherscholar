9c491f08f8e00d974cb688c3ee60ec2b2db9a466
Vattanak Samnang <vsamnang@paragoniu.edu.kh>
2026-03-10 18:16:25 +0700

    Organize files

--- src/main/java/org/example/gatherscholar/repository/MentorRepository.java
+++
@@ -1,7 +0,0 @@
-package org.example.gatherscholar.repository;
-
-import org.example.gatherscholar.entity.Mentor;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MentorRepository extends JpaRepository<Mentor, Long> {
-}
--- src/main/java/org/example/gatherscholar/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package org.example.gatherscholar.repository;
-
-import org.example.gatherscholar.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
